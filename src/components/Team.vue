0f502a03e37b412d83076bd951d63f13f4e2de52
Ajay Williams <31875074+yaja95@users.noreply.github.com>
2018-02-05 01:48:20 -0500

    Add files via upload

---
+++ src/components/Team.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <section>
+      <v-parallax src='static/purp.jpg' height="350">
+        <v-layout column align-center justify-center class="white--text">
+          <h1 class=" white--text darken mb-2 display-3" @click="">Meet Our Team</h1>
+          <h4 class=" white--text headline mb-3 text-xs-center"><em>"Our goal is to take Social Event Sharing to the next level
+               by creating an inclusive application that not only manages the social aspect of events, but the financial aspect
+               of events as well."</em>
+          </h4>
+
+                        
+        </v-layout>
+      </v-parallax>
+    </section>
+<section>
+    <v-container fluid grid-list-md class="grey lighten-4">
+        <v-layout row wrap>
+          <v-flex
+            v-bind="{ [`xs${card.flex}`]: true }"
+            v-for="card in cards"
+            :key="card.title"
+          >
+            <v-card>
+              <v-card-media
+                :src="card.src"
+                height="300px"
+              >
+                <v-container fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                         <v-card-title class="display-1 white--text pl-5 pt-5" v-text="card.title">
+              <!--<div class="display-1 pl-5 pt-5">Ali Conners</div>-->
+            </v-card-title>
+                      <!--<span class="headline white--text" v-text="card.title"></span>-->
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+              <v-card-actions class="purple accent-4 ">
+                <!--<v-spacer></v-spacer>-->
+                <v-btn icon class="text-xl-center">
+                  <v-icon>phone</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mail</v-icon>
+                </v-btn>
+               <!-- <v-btn icon>
+                  <v-icon>share</v-icon>
+                </v-btn>-->
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+</section>
+
+    <section>
+      <v-parallax src='static/purp.jpg' height="300">
+        <v-layout column align-center justify-center class="white--text">
+          <!--<img src="assets/vuetify.png" alt="Vuetify.js">-->
+
+          <!--<v-btn
+                                class="blue lighten-2 mt-5"
+                                dark
+                                large
+                                href="/pre-made-themes"
+                              >
+                                Get Started
+                              </v-btn>-->
+        </v-layout>
+      </v-parallax>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: [],
+      cards: [
+        { title: 'Max Mehlbaum', src: 'static/blue.jpg', flex: 6 },
+        { title: 'Hans Stoetzer', src: 'static/section.jpg', flex: 6 },
+        { title: 'Ajay Williams', src: 'static/section.jpg', flex: 6 },
+        { title: 'Nicholas Yurco', src: 'static/purp.jpg', flex: 6 }
+      ],
+      items: [
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Opportunity One will go here from DB?", text: 'Summer' },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?", text: 'Fall' },
+        { active: true, title: 'Opportunity One', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity One will go here from DB?", text: 'Spring' },
+        { active: true, title: 'Opporunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?", text: 'Winter' },
+        { active: true, title: 'Opportunity One', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity One will go here from DB?" },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?" },
+        { active: true, title: 'Opportunity One', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity One will go here from DB?" },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?" },
+        { active: true, title: 'Opportunity One', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity One will go here from DB?" },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?" },
+        { active: true, title: 'Opportunity One', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity One will go here from DB?" },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?" },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?" },
+        { active: true, title: 'Opportunity One', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity One will go here from DB?" },
+        { active: true, title: 'Opportunity Two', subtitle: "<span class='grey--text text--darken-3'></span> - Description of Opportunity Two will go here from DB?" }
+      ],
+      a1: [],
+      items2: [
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' },
+        { active: true, avatar: 'static/Me.JPG', title: 'Ajay Williams', subtitle: "<span class='grey--text text--darken-3'></span>  Description of Scholar  will go here from DB?", text: 'Summer' }
+      ],
+      categories: [
+        { text: 'Summer' },
+        { text: 'Fall' },
+        { text: 'Spring' },
+        { text: 'Internship' },
+        { text: 'On-Campus' },
+        { text: 'Off-Campus(local)' },
+        { text: 'Off-Campus(non-local)' },
+        { text: 'Paid' },
+        { text: 'Unpaid' },
+        { text: 'Course Credit' },
+        { text: 'Study Away' }
+      ],
+      dialog: false,
+      dialog2: false
+    }
+  },
+  methods: {
+
+    // ///////Method to filter the various events by the selector values\\\\\\\\\\\\\\\\\\\\\\\
+    add () {
+      var green
+      green = this.dialog
+      return green
+    },
+    toggle (index) {
+      const i = this.selected.indexOf(index)
+
+      if (i > -1) {
+        this.selected.splice(i, 1)
+      } else {
+        this.selected.push(index)
+      }
+    }
+  }
+}
+</script>
+
---
+++ src/components/test.vue
@@ -0,0 +1,183 @@
+<template>
+    <!-- <main>-->
+    <v-content>
+      <section>
+        <v-parallax src="static/para.jpg" height="400">
+          <v-layout
+            column
+            align-center
+            justify-center
+            class="white--text"
+          >
+            <img src="static/daplug.png" alt="PlugCoin.io" height="200">
+            <h1 class="white--text mb-2 display-1 text-xs-center">Plug and Plugcoin</h1>
+            <div class="subheading mb-3 text-xs-center">Taking Social Event Sharing to the Next Level</div>
+            <v-btn
+              class="blue lighten-2 mt-5"
+              dark
+              large
+              href="/home"
+            >
+              Get Started
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-5"
+          align-center
+        >
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">The best way to start developing</h2>
+              <span class="subheading">
+                Cras facilisis mi vitae nunc 
+              </span>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Material Design</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Download our App!</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Mine our Coin</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+
+      <section>
+        <v-parallax src="static/section.jpg" height="380">
+          <v-layout column align-center justify-center>
+            <div class="headline white--text mb-3 text-xs-center">Event Sharing has never been easier</div>
+            <em>Delve into Plug!</em>
+            <v-btn
+              class="blue lighten-2 mt-5"
+              dark
+              large
+              href="/pre-made-themes"
+            >
+              Get Started
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-container grid-list-xl>
+          <v-layout row wrap justify-center class="my-5">
+            <v-flex xs12 sm4>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Company info</div>
+                </v-card-title>
+                <v-card-text>
+                 Plug is an event-sharing social media platform that allows users to create events 
+                 and invite their friends or the public. It also allows for cover charges/fees to 
+                 be paid using the PlugCoin currency. </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Contact us</div>
+                </v-card-title>
+                <!--<v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                </v-card-text>-->
+                <v-list class="transparent">
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">phone</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>777-867-5309</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">place</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Greenville, SC</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">email</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>max.mehlbaum@furman.edu</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
+
+      
+    </v-content>
+    <!--</main>-->
+</template>
+
+ <script src="https://unpkg.com/vue/dist/vue.js"></script>
+ <script src="https://unpkg.com/vuetify/dist/vuetify.js"></script>
+  <script>
+        export default {
+          name: 'test',
+          data () {
+            return {
+              msg: 'Welcome to the domain that is Yaja'
+            }
+          }
+        }
+    </script>
